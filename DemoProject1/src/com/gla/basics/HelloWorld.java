0a67b56911ac993b9b3dac34114d099d26ddeb33
manu-saini94 <57988195+manu-saini94@users.noreply.github.com>
2025-06-10 12:54:31 +0530

    Update HelloWorld.java

--- DemoProject1/src/com/gla/basics/HelloWorld.java
+++ DemoProject1/src/com/gla/basics/HelloWorld.java
@@ -2,7 +2,7 @@ package com.gla.basics;
 
 public class HelloWorld{
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World from GITHUB");
 
     }
 }
