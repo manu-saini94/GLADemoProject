55552bb9b807afd30a8cd4ca80ed200c4385ef06
manu <manu.saini94@gmail.com>
2025-06-10 12:49:30 +0530

    second commit

--- DemoProject1/src/com/gla/basics/HelloWorld.java
+++ DemoProject1/src/com/gla/basics/HelloWorld.java
@@ -3,7 +3,5 @@ package com.gla.basics;
 public class HelloWorld{
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("Hello world again updated on GITHUB again");
-        System.out.println("Hello world again updated on LOCAL");
     }
 }
